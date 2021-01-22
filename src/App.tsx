da3e6133da513a43c2216330df24bffb5b619fd9
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-23 00:16:01 +0530

    Types added

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import { fetchQuizQuestion } from './API';
 // components
 import QuestionCard from "./components/QuestionCard";
 // types
-import { Difficulty } from './API';
+import { QuestionState ,Difficulty } from './API';
+
+type AnswerObject = {
+  question : string;
+  answer: string;
+  correct : boolean;
+  correctAnswer : string;
+}
 
 const TOTAL_QUESTIONS = 10; 
 
 function App() {
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true); 
 
